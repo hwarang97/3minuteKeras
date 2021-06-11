f9ec702720196109341502be6675b910a850eb3f
hwarang97 <54321744+hwarang97@users.noreply.github.com>
2021-06-11 22:52:36 +0900

    6/11

--- 3minuteKeras/classification.py
+++ 3minuteKeras/classification.py
@@ -1,42 +1,71 @@
+# 분류 ANN을 위한 인공지능 모델 구현
 from keras import layers, models
 
 
-# 분상형 지정 방식
-x = layers.Input(shape=(Nin,))
-h = layers.Activatoin('relu')(layers.Dense(Nh)(x))
-y = layers.Activation('softmax')(layers.Dense(Nout)(h))
+# 분산 방식 모델링을 포함하는 함수형 구현
+def ANN_models_func(Nin, Nh, Nout):
+    x = layers.Input(shape=(Nin,))
+    h = layers.Activatoin('relu')(layers.Dense(Nh)(x))
+    y = layers.Activation('softmax')(layers.Dense(Nout)(h))
+    model = models.Model(x, y)
+    model.compile(loss='categorical_crossentropy', optimizer = 'adam', metrics=['accuracy'])
 
-model.compile(loss='categorical_crossentropy', optimizer = 'adam', metrics=['accuracy'])
+    return model
 
+# 연쇄 방식 모델링을 포함하는 함수형 구현
+def ANN_seq_func(Nin, Nh, Nout):
+    model = models.Sequential()
+    model.add(layers.Dense(Nh, activation='relu', input_shaep=(Nin,))) # 입력과 은닉이 동시에 정해짐
+    model.add(layers.Dense(Nout, activation='softmax'))
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
-# 연쇄형 지정 방식
-model = models.Sequential()
+    return model
 
-model.add(layers.Dense(Nh, activation='relu', input_shaep=(Nin,))) # 입력과 은닉이 동시에 정해짐
-model.add(layers.Dense(Nout, activation='softmax'))
-
-# 객체 지향형 구현
-class ANN(models.Model):
+# 분산 방식 모델링을 포함하는 객체지향형 구현
+class ANN_models_class(models.Model):
     def __init__(self, Nin, Nh, Nout):
+        # Prepare network layers and activate functions
+        hidden = layers.Dense(Nh)
+        output = layers.Dense(Nout)
+        relu = layers.Activation('relu')
+        softmax = layers.Activation('softmax')
 
-hidden = layers.Dense(Nh)
+        # Connect network elements
+        x = layers.Input(shape=(Nin,))
+        h = relu(hidden(x))
+        y = softmax(output(h))
 
+        super().__init__(x, y)
+        self.complie(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+# 연쇄 방식 모델링을 포함하는 객체지향형 구현
+class ANN_seq_class(models.Sequential):
+    def __init__(self, Nin, Nh, Nout):
+        super().__init__()
+        self.add(layers.Dense(Nh, activation='relu', input_shape=(Nin,)))
+        self.add(layers.Dense(Nout, activation='softmax'))
+        self.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+# 분류 ANN에 사용할 데이터 불러오기
 import numpy as np
+import tensorflow as tf # tf를 써주지 않으니까 데이터를 가져오는데 오류가 발생한다.
 from keras import datasets #mnist
 from keras.utils import np_utils #to_categorical
 
-(X_train, y_train), (X_test, y_test) = datasets.mnist.load_data()
+def Data_func():
+    (X_train, y_train), (X_test, y_test) = tf.keras.datasets.mnist.load_data()
 
-Y_train = np_utils.to_categorical(y_train) # 0~9 정수를 이진 벡터로 변환, 효율적이라서
-Y_test = np_utils.to_categorical(y_test)
+    Y_train = np_utils.to_categorical(y_train) # 0~9 정수를 이진 벡터로 변환, 효율적이라서
+    Y_test = np_utils.to_categorical(y_test)
 
-L, W, H = X_train.shape
-X_train = X_train.reshape(-1, W*H) # L개의 이미지를 저장 ( L * W * H )
-X_test = X_test.reshape(-1, W*H)
+    L, W, H = X_train.shape
+    X_train = X_train.reshape(-1, W*H) # L개의 이미지를 저장 ( L * W * H )
+    X_test = X_test.reshape(-1, W*H)
 
-X_train = X_train/ 255.0 # 각 이미지의 구성된 값을 0~1사이의 값으로 변환
-X_test = X_test / 255.0
+    X_train = X_train/ 255.0 # 각 이미지의 구성된 값을 0~1사이의 값으로 변환
+    X_test = X_test / 255.0
 
+    return (X_train, Y_train), (X_test, Y_test)
 
 import matplotlib.pyplot as plt
 
@@ -61,3 +90,22 @@ def main():
     Nh = 100
     number_of_class = 10
     Nout = number_of_class
+
+    model = ANN_seq_class(Nin, Nh, Nout)
+    (X_train, Y_train), (X_test, Y_test) = Data_func() # 가공한 데이터를 받아오기
+
+    #################################
+    # Training
+    #################################
+    history = model.fit(X_train, Y_train, epochs=15, batch_size=100, validation_split=0.2)
+    performance_test = model.evaluate(X_test, Y_test, batch_size=100)
+    print('Test Loss and Accuracy ->', performance_test)
+
+    plot_loss(history)
+    plt.show()
+    plot_acc(history)
+    plt.show()
+
+# Run code
+if __name__ == '__claasification__':
+    main()
---
+++ 3minuteKeras/main.py
@@ -0,0 +1,3 @@
+import classification
+
+classification.main()
